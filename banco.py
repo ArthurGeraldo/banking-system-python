c4c2773c217bddbca3c77eba757b208df41cb6c7
ArthurGeraldo <arthurgeraldo718@gmail.com>
2026-02-06 00:58:27 -0300

    polimento do codigo

--- banco.py
+++ banco.py
@@ -1,7 +1,8 @@
-#ATRIBUTOS: TITULAR, SALDO
-#METODOS: DEPOSITAR, SACAR, MOSTRAR_SALDO
+#IMPORTAÇÃO
 import os
 import json
+
+#CLASSE DO BANCO
 
 class Banco():
     def __init__(self, titular, saldo):
@@ -9,7 +10,7 @@ class Banco():
         self.saldo = saldo
     def depositar(self, valor):
         self.saldo += valor
-        print(f"Valor depositado, Saldo atual: {self.saldo} R$")
+        print(f"Valor depositado.\nSaldo atual: {self.saldo} R$")
     def sacar(self,valor):
         if valor > self.saldo:
             print("*SALDO INSUFICIENTE*")
@@ -26,6 +27,8 @@ class Banco():
         with open("conta.json", "w", encoding="utf-8") as arquivo:
             json.dump(dados, arquivo, indent=4, ensure_ascii=False)
 
+#FUNÇÃO
+    #CARREGAR CONTA EXISTENTE
 def carregar_conta():
         try:
             with open("conta.json", "r", encoding="utf-8") as arquivo:
@@ -33,15 +36,17 @@ def carregar_conta():
                 return Banco(dados["titular"], dados["saldo"])
         except FileNotFoundError:
             return None
-
+    #LIMPAR TERMINAL
 def limpar():
     os.system("cls")
+    #VOLTAR PARA MENU
 def voltar():
     print("*ENTER PARA VOLTAR*")
     input("")
 
+#CARREGAMENTO DA CONTA
 conta = carregar_conta()
-
+#MENU
 while True:
     try:
         limpar()
@@ -54,42 +59,47 @@ while True:
             """)
             nome_usuario = str(input(f"{">".center(5)}"))
             conta = Banco(nome_usuario, 0)
-        limpar()
-        print(f"""
-        {"-"*30}
-        {f"BEM VINDO, {conta.titular.upper()}".center(30)}
-        {"CONTA BANCÁRIA".center(30)}
-        {"-"*30}
-        {"1 -DEPOSITAR-"}
-        {"2 -SACAR-"}
-        {"3 -SALDO-"}
-        """)
-        resposta = input(f"{">".center(5)}")
-        #DEPOSITO
-        if resposta.lower() == "depositar" or "1":
+        else:
             limpar()
-            print("Insira valor para depositar.")
-            valor = int(input(f"{">".center(5)}"))
-            conta.depositar(valor)
-            conta.salvar()
-            voltar()
-        #SACAR
-        if resposta.lower() == "sacar" or "2":
-            limpar()
-            print("Insira valor para saque.")
-            conta.mostrar_saldo()
-            if conta.saldo == 0:
-                voltar()
-            else:
+            print(f"""
+            {"-"*30}
+            {f"BEM VINDO, {conta.titular.upper()}".center(30)}
+            {"CONTA BANCÁRIA".center(30)}
+            {"-"*30}
+            {"1 -DEPOSITAR-"}
+            {"2 -SACAR-"}
+            {"3 -SALDO-"}
+            """)
+            resposta = input(f"{">".rjust(5)}")
+
+            #DEPOSITO
+            if resposta.lower() in ("depositar" "1"):
+                limpar()
+                print("Insira valor para depositar.")
                 valor = int(input(f"{">".center(5)}"))
-                conta.sacar(valor)
+                conta.depositar(valor)
                 conta.salvar()
                 voltar()
-        #MOSTRAR SALDO
-        if resposta.lower() == "saldo" or "3":
-            limpar()
-            conta.mostrar_saldo()
-            voltar()
+            
+            #SACAR
+            elif resposta.lower() in ("sacar", "2"):
+                limpar()
+                print("Insira valor para saque.")
+                conta.mostrar_saldo()
+                if conta.saldo == 0:
+                    voltar()
+                else:
+                    valor = int(input("> "))
+                    conta.sacar(valor)
+                    conta.salvar()
+                    voltar()
+            
+            #MOSTRAR SALDO
+            elif resposta.lower() in ("saldo", "3"):
+                limpar()
+                conta.mostrar_saldo()
+                voltar()
+    #EXCEPT
     except ValueError:
         print("Digite um valor válido.")
         voltar()
